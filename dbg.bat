2d6aced4c1ef0e28e74fe6e6171443cc7a57da96
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2020-04-02 17:50:15 -0400

    Updated for debugging IMAGE_DISP.

--- dbg.bat
+++ dbg.bat
@@ -6,10 +6,10 @@ rem   Build the program in debug mode, then debug it.  The optional arguments
 rem   are passed to the program when run in the debugger.
 rem
 setlocal
-set prog=image_ovl
+set prog=image_disp
 set debug_vs=true
 set debugging=true
 call build
 if errorlevel 1 goto :eof
 call extpath_var msvc/debugger.exe tnam
-"%tnam%" /DebugExe %prog%.exe /img/x -out img/y.jpg %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%tnam%" /DebugExe %prog%.exe /img/x -dev debug %1 %2 %3 %4 %5 %6 %7 %8 %9
