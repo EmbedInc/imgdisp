2a11baed9db310f412faa1a520f325d34ab71142
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2020-10-30 18:34:51 -0400

    Git now ignores debug scripts.

--- dbg.bat
+++ dbg.bat
@@ -11,5 +11,7 @@ set debug_vs=true
 set debugging=true
 call build
 if errorlevel 1 goto :eof
+copyt /img/5008.jpg /img/t.jpg
+copya /img/5008.displ /img/t.displ
 call extpath_var msvc/debugger.exe tnam
-"%tnam%" /DebugExe %prog%.exe /img/x -dev debug %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%tnam%" /DebugExe %prog%.exe /img/t.jpg -dev debug %1 %2 %3 %4 %5 %6 %7 %8 %9
